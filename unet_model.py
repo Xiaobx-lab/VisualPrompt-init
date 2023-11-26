ecbd07a62abcf3828d80abd152aaefb9bdcc3803
Xiaobx-lab <57618171+Xiaobx-lab@users.noreply.github.com>
2023-11-26 13:55:32 +0800

    Update unet_model.py

--- unet_model.py
+++ unet_model.py
@@ -7,38 +7,6 @@ from torch import nn
 
 from unet_parts import DoubleConv, Down, Up, OutConv
 
-class PALayer(nn.Module):
-    def __init__(self, channel):
-        super(PALayer, self).__init__()
-        self.pa = nn.Sequential(
-            nn.Conv2d(channel, channel // 8, 1, padding=0, bias=True),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(channel // 8, 1, 1, padding=0, bias=True),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        y = self.pa(x)
-        return x * y
-
-
-class CALayer(nn.Module):
-    def __init__(self, channel):
-        super(CALayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.ca = nn.Sequential(
-            nn.Conv2d(channel, channel // 8, 1, padding=0, bias=True),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(channel // 8, channel, 1, padding=0, bias=True),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        y = self.avg_pool(x)
-        y = self.ca(y)
-        return x * y
-
-
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes,bilinear=True):
         super(UNet, self).__init__()
